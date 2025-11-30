b356dc2dd00d326cef6dbd3e121ef12fdf3435cd
Tiến Phát <Tienphat040322@gmail.com>
2025-11-30 11:48:07 +0700

    add cart context

---
+++ frontend/src/contexts/CartContext.tsx
@@ -0,0 +1,132 @@
+import { createContext, useContext, useState, type ReactNode } from "react";
+
+interface CartItem {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+  selected?: boolean;
+}
+
+interface CartContextType {
+  items: CartItem[];
+  addToCart: (product: Omit<CartItem, "selected">) => void;
+  removeFromCart: (id: string) => void;
+  updateQuantity: (id: string, quantity: number) => void;
+  toggleSelect: (id: string) => void;
+  toggleSelectAll: () => void;
+  clearCart: () => void;
+  getSelectedItems: () => CartItem[];
+  getTotalPrice: () => number;
+  getSelectedTotalPrice: () => number;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [items, setItems] = useState<CartItem[]>([]);
+
+  const addToCart = (product: Omit<CartItem, "selected">) => {
+    setItems((prev) => {
+      //kiểm tra xem item đã tồn tại chưa, nếu rồi thì cộng dồn số lượng
+      //nếu chưa thì thêm mới
+      const existingItem = prev.find((item) => item.id === product.id);
+      if (existingItem) {
+        //cộng dồn
+        const tmpItems = prev.map((item) => {
+          if (item.id === product.id)
+            return { ...item, quantity: item.quantity + product.quantity };
+          return item;
+        });
+        return tmpItems;
+      }
+      //thêm mới
+      return [...prev, { ...product, selected: true }];
+    });
+  };
+
+  const removeFromCart = (id: string) => {
+    setItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const updateQuantity = (id: string, quantity: number) => {
+    if (quantity <= 0) {
+      removeFromCart(id);
+      return;
+    }
+    setItems((prev) => {
+      return prev.map((item) => {
+        if (item.id === id) return { ...item, quantity };
+        return item;
+      });
+    });
+  };
+
+  const toggleSelect = (id: string) => {
+    setItems((prev) => {
+      return prev.map((item) => {
+        if (item.id === id) return { ...item, selected: !item.selected };
+        return item;
+      });
+    });
+  };
+
+  const toggleSelectAll = () => {
+    //kiểm tra xem có đang select all không
+    //nếu select all rồi thì sẽ cho phép unselect all
+    //nếu chưa select all (select 0 hoặc many) thì cho phép select all
+    const isAllSelected = items.every((item) => item.selected);
+    setItems((prev) => {
+      return prev.map((item) => {
+        return { ...item, selected: !isAllSelected };
+      });
+    });
+  };
+
+  const clearCart = () => {
+    setItems(() => []);
+  };
+
+  const getSelectedItems = () => {
+    return items.filter((item) => item.selected);
+  };
+
+  const getTotalPrice = () => {
+    return items.reduce((total, item) => total + item.price * item.quantity, 0);
+  };
+
+  const getSelectedTotalPrice = () => {
+    const selectedItems = items.filter((item) => item.selected);
+    return selectedItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        items,
+        addToCart,
+        removeFromCart,
+        updateQuantity,
+        toggleSelect,
+        toggleSelectAll,
+        clearCart,
+        getSelectedItems,
+        getTotalPrice,
+        getSelectedTotalPrice,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (context === undefined)
+    throw new Error("useCart must be used within a CartProvider");
+  return context;
+};
