2fba502a78709e48243fc3bec5ca555bc778d9ad
Tiến Phát <Tienphat040322@gmail.com>
2025-12-02 17:10:53 +0700

    add db entity

---
+++ backend/YenSaoCaoCap-Be/Domain/Entities/RefreshToken.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class RefreshToken : BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string HashedToken { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; } = DateTime.UtcNow.AddDays(7);
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+        private RefreshToken() { }
+        public RefreshToken(string hashedToken, DateTime expiresAt, User user)
+        {
+            HashedToken = hashedToken;
+            ExpiresAt = expiresAt;
+            User = user;
+        }
+    }
+}
---
+++ backend/YenSaoCaoCap-Be/Domain/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string HashedPassword { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public List<RefreshToken> RefreshTokens { get; set; } = new();
+        private User() { }
+        public User(string email, string phoneNumber, string hashedPassword, string firstName, string lastName)
+        {
+            Email = email;
+            PhoneNumber = phoneNumber;
+            HashedPassword = hashedPassword;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
