487ab407cd15b3ff2d6ba9e0464099ca53dfb78e
Tiến Phát <Tienphat040322@gmail.com>
2025-12-03 11:58:34 +0700

    db and auto migration

--- backend/YenSaoCaoCap-Be/API/Program.cs
+++ backend/YenSaoCaoCap-Be/API/Program.cs
@@ -1,3 +1,10 @@
+
+using DotNetEnv;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +12,65 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new() { Title = "API Gateway", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter token"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+//database
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
+
+//auto migrate and seed database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    var retries = 5;
+    for (int i = 0; i < retries; i++)
+    {
+        try
+        {
+            // Create DB if it doesnt exist and apply all migrations
+            await db.Database.MigrateAsync();
+            Console.WriteLine("Database migrated and seeded successfully.");
+            break;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine($"Database not ready, retrying in 5s... ({i + 1}/{retries})");
+            await Task.Delay(5000);
+            if (i == retries - 1) throw;
+        }
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
