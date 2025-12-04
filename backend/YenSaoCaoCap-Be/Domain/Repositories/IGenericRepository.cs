676566f6d22a661c119473e6daa79f1e2f05d3d4
Tiến Phát <Tienphat040322@gmail.com>
2025-12-04 10:20:46 +0700

    generic repo

---
+++ backend/YenSaoCaoCap-Be/Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(bool isTracking = false);
+        Task<T?> GetByIdAsync(Guid id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ backend/YenSaoCaoCap-Be/Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Repositories;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<List<T>> GetAllAsync(bool isTracking = false)
+        {
+            if (isTracking)
+                return await _dbSet.ToListAsync();
+            return await _dbSet.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
